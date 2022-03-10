d33764b38cd316e02ae12ffe446f70a0efe9f336
SlayerWx <maxi_cs_95@hotmail.com>
2022-03-10 20:46:32 -0300

    WeaponInput using UnityEvents

---
+++ Assets/Maxi/Script/WeaponInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class WeaponInput : MonoBehaviour
+{
+    public KeyCode shootInput;
+    public KeyCode reloadInput;
+    public KeyCode changeWeaponInput;
+    public UnityEvent OnShoot;
+    public UnityEvent OnStopShoot;
+    public UnityEvent OnReload;
+    public UnityEvent OnchangeWeapon;
+    void Update()
+    {
+        PlayerInput();
+    }
+    void PlayerInput()
+    {
+
+        if (Input.GetKeyDown(shootInput))
+        {
+            OnShoot.Invoke();
+        }
+        else if (Input.GetKeyUp(shootInput))
+        {
+            OnStopShoot.Invoke();
+        }
+        else if (Input.GetKeyDown(reloadInput))
+        {
+            OnReload.Invoke();
+        }
+        else if (Input.GetKeyDown(changeWeaponInput))
+        {
+            OnchangeWeapon.Invoke();
+        }
+    }
+}
