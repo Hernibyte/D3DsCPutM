e5127abe09e0968f0727c0d33fb7c1ba353dddbe
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-11 01:51:03 -0300

    Added a player movement

---
+++ Assets/Hernan/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10;
+    [SerializeField] float jumpForce = 3f;
+    [SerializeField] float worldGravity = -9.81f;
+    [SerializeField] Transform basePoint;
+    [SerializeField] LayerMask floorLayerMask;
+
+    CharacterController controller;
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 newPosition = (transform.right * x + transform.forward * y) * movementSpeed * Time.deltaTime;
+
+        controller.Move(newPosition);
+    }
+
+    void Jump()
+    {
+        isGrounded = Physics.CheckBox(basePoint.position, new Vector3(0.8f, 1f, 0.8f), Quaternion.identity, floorLayerMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * worldGravity);
+
+        velocity.y += worldGravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
