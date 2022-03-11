4f3deafadb47999f38f608e15a75f63e6c02dab5
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-11 17:22:30 -0300

    Added a weapon generator

---
+++ Assets/Hernan/Scripts/Shop/WeaponGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGenerator : MonoBehaviour
+{
+    public List<GameObject> weaponList;
+
+    public void GenerateWeapon(int index, Transform parent, Inventory inventory)
+    {
+        inventory.primaryWeapon = Instantiate(weaponList[index], parent);
+    }
+}
