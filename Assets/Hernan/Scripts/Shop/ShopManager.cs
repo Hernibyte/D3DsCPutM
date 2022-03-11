dc30273798af83c78b160a16dc29e424f3d1ccb2
Gossa Hernan <hernanagustin.g24@hotmail.com>
2022-03-11 17:21:34 -0300

    Added a shop manager [intermediary]

---
+++ Assets/Hernan/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public PlayerInventoryController inventoryController;
+    public WeaponGenerator generator;
+
+    void Start()
+    {
+        inventoryController.e_WeaponRequest.AddListener(generator.GenerateWeapon);
+    }
+}
